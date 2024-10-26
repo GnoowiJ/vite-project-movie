bbf35cb0322f4d3f0f1910d75ee3be7cf0291e12
웅's <qowldnd002@naver.com>
2024-10-26 20:32:29 +0900

    browserrouter fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import Header from './components/Header.jsx';
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <div className='app'>
         <Routes>
-          <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
+          <Route path='/' element={<Home />} />
           <Route path='/movie/:id' element={<Detail />} />
         </Routes>
       </div>
