b6a0ef4b2784611450251e5d8e0b4da135457e27
웅's <qowldnd002@naver.com>
2024-10-29 17:31:21 +0900

    10/29 last

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 export default function Header () {
   return (
     <div className='header'>
-      <h3 className='nav-title'>국내 박스 오피스 top 10!</h3>
+      <Link to='/'>
+        <h3 className='nav-title'>국내 박스 오피스 top 10!</h3>
+      </Link>
     </div>
   );
 }
